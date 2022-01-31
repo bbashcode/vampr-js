26ac6ec70332a6b4ae34aa273b4fd71fe989d8a1
bbashcode <binbash.code@gmail.com>
2022-01-31 05:27:20 +0000

    passing 3 tests as of now

--- vampire.js
+++ vampire.js
@@ -10,16 +10,26 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    // offspring.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVampires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numOfVampires++;
+    }
+
+    return numOfVampires;
 
   }
 
